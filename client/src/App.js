04ec93d5dc64689ed70cfc248b0c3dd922bdd7c5
Mandell Butler <mandell.butler@outlook.com>
2021-06-23 12:26:57 -0400

    Import context and providers to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import AuthenticatedApp from './components/AuthenticatedApp';
+import UnauthenticatedApp from './components/UnauthenticatedApp';
+import { GlobalProvider } from "./context/GlobalContext"
 import './App.css';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const login = () => setLoggedIn(true);
+
+  const logout = () => setLoggedIn(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+
     </div>
   );
 }
