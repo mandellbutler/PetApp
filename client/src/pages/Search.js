428dd55b154865dc07de627548e5db3ad87d1d56
Jeff Lebowski <anentitymusic@gmail.com>
2021-06-24 13:22:15 -0400

    added search form

---
+++ client/src/pages/Search.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useGlobalContext } from '../context/GlobalContext';
+
+function SearchPage() {
+  const [state, dispatch] = useGlobalContext();
+  return (
+    <div>
+       <form action="/action_page.php"/>
+       <h2>Fetch Friends By:</h2>
+        <label for="location">Location:</label><br></br>
+        <input type="text" id="location" name="location"/><br></br>
+        <label for="breed">Breed:</label><br></br>
+        <input type="text" id="breed" name="breed"/><br></br>
+        <button>Fetch Friend!</button>
+    </div>
+  )
+}
+
+export default SearchPage
