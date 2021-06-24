9518df45c3669654dc10f56bc85de7609c96539a
Jeff Lebowski <anentitymusic@gmail.com>
2021-06-24 14:34:05 -0400

    added password  shit to models

--- server/models/Human.js
+++ server/models/Human.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const humanSchema = new Schema(
   {
@@ -18,17 +19,30 @@ const humanSchema = new Schema(
     },
     email: {
       type: String,
-      required: true
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match an email address!'],
     },
     password: {
       type: String,
-      allowNull: false,
-      validate: {
-        len: [8]
-      }
+      required: true,
+      minlength: 5,
+    },
   }
-}
 )
+
+humanSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+humanSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const Human = model('Human', humanSchema)
 
