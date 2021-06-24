8b47a0533b20c9fb26dba025d0375068e6b42645
Mandell Butler <mandell.butler@outlook.com>
2021-06-24 19:43:53 -0400

    Modified client side app.js for login with JWT, cache, and grpahql uri

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,44 @@
 import AuthenticatedApp from './components/AuthenticatedApp';
 import UnauthenticatedApp from './components/UnauthenticatedApp';
 import { useGlobalContext } from "./context/GlobalContext"
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import './App.css';
 import React, { useState } from "react";
-import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
-const client = new ApolloClient({
-  request: operation => {
-    const token = localStorage.getItem('id_token');
+//construct main graphql endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
-    })
-  },
-  uri: '/graphql'
+//construct requesrt middleware that will attach the JWT to every request as an  'authorization
+const authLink = setContext((_, { headers }) => {
+  //get the authentication token from local storage (if it exists)
+  const token = localStorage.getItem('id_token');
+  //return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  //set up our client to execute the 'authLink' middleware prior to making the request to Graphql API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 function App() {
   const [state, dispatch] = useGlobalContext();
-
-
-
 
   return (
 
