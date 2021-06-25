facf69b0469a1fbfce26aee13ee2bed38a954105
Hawa <hawa.francis@gmail.com>
2021-06-25 07:54:49 -0700

    card

--- client/src/pages/PetProfile.js
+++ client/src/pages/PetProfile.js
@@ -16,29 +16,7 @@ const SavedPets = () => {
 
   return (
     <>
-{/* userdata.savedpets */}
-{/* <Row>
-{data.savedPets.map((dog) => {
-            return (
-<Col s={12} m={7}>
-      <Card>
-        <div className="card-image"> */}
-        {/* <img src={dog.image} alt="pet-profile-pic" /> */}
-          {/* <span className="card-title">{dog.name}</span>
-        </div>
-        <div className="card-content">
-            <h3>My name is {dog.name}, welcome to me profile!</h3>
-            <p>Breed:{dog.breed}</p>
-          <p>Location:{dog.location}</p>
-        </div>
-        <div className="card-action"> */}
-          {/* <a href="#">This is a link</a> */}
-        {/* </div>
-      </Card>
-    </Col> */}
-    );
-            {/* })}; */}
-{/* </Row> */}
+
 <Row>
   <Col
     m={6}
@@ -49,7 +27,7 @@ const SavedPets = () => {
         <a key="1" href="#">This is a Link</a>
       ]}
       closeIcon={<Icon>close</Icon>}
-      header={<CardTitle image="https://materializecss.com/images/sample-1.jpg">Card Title</CardTitle>}
+      header={<CardTitle>Card Title</CardTitle>}
       revealIcon={<Icon>more_vert</Icon>}
     >
       Here is the standard card with an image thumbnail.
