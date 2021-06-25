380f757d6a1a6174fd125e2517810fe2b742fa7f
Jeff Lebowski <anentitymusic@gmail.com>
2021-06-25 11:15:00 -0400

    commmmitt

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,18 +8,17 @@ const NavBar = () => {
   return (
    
     
-  //   <nav>
-  //   <div className="nav-wrapper">
-  //     {/* <a href="#" class="brand-logo">Logo</a> */}
-  //     <Ul id="nav-mobile" class="right hide-on-med-and-down">
-  //       <Li><a href="/adoption-page">Adoption Page</a></Li>
-  //       <Li><a href="/human-profile">My Profile</a></Li>
-  //       <Li><a href="/pet-profile">Pet Profile</a></Li>
-  //       <Li><a href="/search">Search</a></Li>
-  //     </Ul>
-  //   </div>
-  // </nav>
-<p>nvm</p>
+    <nav>
+    <div className="nav-wrapper">
+      {/* <a href="#" class="brand-logo">Logo</a> */}
+      <Ul id="nav-mobile" class="right hide-on-med-and-down">
+        <Li><a href="/adoption-page">Adoption Page</a></Li>
+        <Li><a href="/human-profile">My Profile</a></Li>
+        <Li><a href="/pet-profile">Pet Profile</a></Li>
+        <Li><a href="/search">Search</a></Li>
+      </Ul>
+    </div>
+  </nav>
   );
 }
 
