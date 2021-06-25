f356b5c05aa18b710fb46eda2d46fe8be7e8765d
Yilun Xiao <yilunxiao2@gmail.com>
2021-06-25 14:31:58 -0400

    save

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -17,7 +17,7 @@ const Home = () =>{
         <a key="1" href="#">This is a Link</a>
       ]}
       closeIcon={<Icon>close</Icon>}
-      header={<CardTitle image src={homedog}>Card Title</CardTitle>}
+      header={<CardTitle image src={homedog}></CardTitle>}
       revealIcon={<Icon>more_vert</Icon>}
     >
       Here is the standard card with an image thumbnail.
--- client/src/pages/PetProfile.js
+++ client/src/pages/PetProfile.js
@@ -27,7 +27,7 @@ const SavedPets = () => {
         <a key="1" href="#">This is a Link</a>
       ]}
       closeIcon={<Icon>close</Icon>}
-      header={<CardTitle>Card Title</CardTitle>}
+      header={<CardTitle></CardTitle>}
       revealIcon={<Icon>more_vert</Icon>}
     >
       Here is the standard card with an image thumbnail.
