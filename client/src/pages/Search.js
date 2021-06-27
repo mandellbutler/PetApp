489ea677d0f7663306dc751b3a42134375c17ad4
Yilun Xiao <yilunxiao2@gmail.com>
2021-06-26 20:05:44 -0400

    modified search page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -3,13 +3,12 @@ import { useGlobalContext } from '../context/GlobalContext';
 import PetList from '../components/PetList';
 
 
+
 function Search() {
   // const [state, dispatch] = useGlobalContext();
 
   // url and token for fetch request
-  const url = "https://api.petfinder.com/v2/animals?location=ny";
-  const access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI2cEZiSWx0bGRCOUZ4c2plaHRlSmwwbThOc3JSaGJDRkxJTHM4ZUd0WVBtWWdzOEhodyIsImp0aSI6IjAwNzhmODUwMzZhNzkwY2NlNzFhNTY5ZjViZmRhNmM0MTBkMzc0MmZhZjJmODFiMmNhZTYxYzUzZjFhNTU5YzhiYjEyNmIzZjZmOGVmNzc5IiwiaWF0IjoxNjI0NjM2ODQ3LCJuYmYiOjE2MjQ2MzY4NDcsImV4cCI6MTYyNDY0MDQ0Nywic3ViIjoiIiwic2NvcGVzIjpbXX0.vnnwYn9fofrueUjrmFyR_g6tpp5425qLFGU9naDU-9tUZ-09W5nszn3G5eiHEG4mAhVQFFbULjytGiJsuCg7Cs3ntzN6-5IKUxSRMczAwQjYZPHAX3mu5prugIXljrM-lXydc0scPRiRTCLJNHIi9itkMz0hAUEAQOw18hhYAPuNECJICFLcehKhRGYce3C8TkTT5Y9-aUcCwZIrSMFRcIBKcqOS8gn4mXTmh9ECCC35YDJjflzhGdPDpYD3JHgUapuDuozuTL99vXqsIi79I3nHg0fQedZUm7EFdLrthSqe7IH3xotcaSTQ7PqAkJEU1PlugR2Ki45k7K2QClj-qQ";
-  // Starting pets
+  const url = "https://api.petfinder.com/v2/animals?location=ny";  // Starting pets
   const initialPets = [
     {
       name: 'steve',
@@ -49,18 +48,50 @@ function Search() {
   const [pets, setPets] = useState(initialPets);
 
   // Fetch api info from petfinder
-  const getPets = async (url, token) => {
-    console.log("Run getPets()");
-    const response = await fetch(url, {
-        // add token
-        headers: {
-            'Authorization': 'Bearer '+token, 
-        }
+  const getPets = async () => {
+    let token = process.env.REACT_APP_APITOKEN;
+    console.log("Tolkein: " + token);
+    let apiData = await getApi(url, token);
+
+    // if token doesn't work, get new one
+    if (apiData.status !== 200) {
+        token = await getToken();
+        apiData = await getApi(url, token)
+    }
+
+    console.log("PETS API ", apiData)
+    setPets([...pets, ...apiData.animals]);
+  }
+
+  const getApi = async (url, token) => {
+    const data = await fetch(url, {
+      // add token
+      headers: {
+          'Authorization': 'Bearer '+token, 
+      }
     })
-    console.log("Response: ", response);
-    const data = await response.json();
-    console.log("PETS API ", data)
-    setPets([...pets, ...data.animals]);
+    const response = await data.json();
+    // console.log(response);
+    return response;
+  }
+
+  const getToken = async () => {
+    const cred = JSON.stringify({
+      grant_type: "client_credentials",
+      client_id: process.env.REACT_APP_APIKEY,
+      client_secret: process.env.REACT_APP_APISECRET
+    })
+    // Get token from petfinder api
+    const data = await fetch("https://api.petfinder.com/v2/oauth2/token", {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: cred,
+        method: "POST"
+    })
+    const response = await data.json();
+    console.log(response);
+    return response
   }
 
   return (
@@ -71,7 +102,7 @@ function Search() {
         <input type="text" id="location" name="location"/><br></br>
         <label for="breed">Breed:</label><br></br>
         <input type="text" id="breed" name="breed"/><br></br>
-        <button onClick={()=>getPets(url, access_token)}>Fetch Friends!</button>
+        <button onClick={()=>getPets()}>Fetch Friends!</button>
 
       <PetList pets={pets}></PetList>
 
